b893d14f44f06b6922c0fa7e94ed350bd8847855
Mick Ordway <mickordway@gmail.com>
2018-10-27 04:43:55 -0400

    updated an exercise from EloquentJS

--- eloquentJS/ch2/chessboard.js
+++ eloquentJS/ch2/chessboard.js
@@ -1,8 +1,22 @@
 "use strict"
 
-let size = 8;
+let size;
 let s = '';
 
-for (let i = 1; i <= size; i++) {
+if (process.argv.length > 2) {
+    size = process.argv[2];
+} else {
+    size = 8;
+}
 
+for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= size; j++) {
+        if (i % 2 === 1){
+            s += (j % 2 === 1) ? " " : "#";
+        } else {
+            s += (j % 2 === 1) ? "#" : " ";
+        }
+    }
+    console.log(s);
+    s = '';
 }
